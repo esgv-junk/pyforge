57bb84681668d42e60c51b6e590fb14dec16cdb8
esgv <ecxsgv@gmail.com>
2011-11-20 02:45:41 +0400

    Removed `escaped` and `descaped`, because it's impossible to figure out which way should the dictionary be. Gonna use `multiple_replace`.
    Probably need `multiple_replace_re`.

--- str_utils.py
+++ str_utils.py
@@ -15,14 +15,9 @@ def multiple_replace(string, replace_dict):
     return re.sub(
         make_strings_re(replace_dict.keys()),
         lambda match_obj: replace_dict[match_obj.group(0)],
-        string)
+        string
+    )
     
-def escaped(string, descape_dict):
-    return multiple_replace(string, reversed_dict(descape_dict))
-    
-def descaped(string, descape_dict):
-    return multiple_replace(string, descape_dict)
-
 escaped_text_re = r'(?:[^\\]|\\.)+?'
 
 def to_id(string):
